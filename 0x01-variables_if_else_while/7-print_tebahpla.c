a7a09002fb298572e79bf8e6099f30040aa934b0
Ibibo Manuel <manuel.ibibo@yahoo.com>
2022-12-11 13:56:24 -0800

    smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in revese
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
