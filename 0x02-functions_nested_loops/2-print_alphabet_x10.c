3c9062e5ac81074b7599b4b4b868d22100ea8a61
Ibibo Manuel <manuel.ibibo@yahoo.com>
2022-12-13 21:13:23 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet 10 timesi in lowercase
+ * print_alphabet - print alphabet 10 times in lowercase
  *
  */
 void print_alphabet_x10(void)
@@ -14,7 +14,6 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
-		return (0);
 	}
 
 }
