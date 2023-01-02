fb8ec262a7f4c66f6284cd4fa2551ad8b983f716
Ibibo Manuel <manuel.ibibo@yahoo.com>
2023-01-02 13:56:59 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int index;
 
 	while (*s)
 	{
-		for (i = 0; accept[i]; i++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[i])
+			if (*s == accept[index])
 				return (s);
 		}
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }
