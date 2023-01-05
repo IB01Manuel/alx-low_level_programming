72051b7df6e738ccd61475988331f87b115a555a
Ibibo Manuel <manuel.ibibo@yahoo.com>
2023-01-05 12:25:17 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,11 +7,20 @@
  */
 int factorial(int n)
 {
-	int result = n;
+	int x;
 
-	if (n < 0)
-		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
+	{
 		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+	return (x);
+
 }
