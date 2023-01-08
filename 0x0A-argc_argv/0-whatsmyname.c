c2b19bd9b8c83296806b70c9de53ac6b1d20b62b
Ibibo Manuel <manuel.ibibo@yahoo.com>
2023-01-08 14:16:08 -0800

    whats_my_name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument counts
+ * @argv: argument string_array
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
