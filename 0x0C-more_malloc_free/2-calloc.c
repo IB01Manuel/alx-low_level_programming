9c91c81498c8d7639acc66e3ae810edc4d1ba4b1
Ibibo Manuel <manuel.ibibo@yahoo.com>
2023-01-13 20:33:46 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	f = malloc(nmemb * size);
-	if (a == NULL)
+	if (f == NULL)
 		return (NULL);
 	for (g = 0; g < (nmemb * size); g++)
 		f[g] = 0;
